da73108942c10ca2612e5bd137dceaa58e9d50f8
Samuel <samuelketema01@gmail.com>
2024-04-05 10:41:51 +0300

    State is managed by using StreamBuilder widget

--- lib/src/Screens/loginApp.dart
+++ lib/src/Screens/loginApp.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/material.dart';
+import '../blocs/bloc.dart';
 
 class LoginApp extends StatelessWidget {
   Widget build(context){
@@ -7,32 +8,43 @@ class LoginApp extends StatelessWidget {
       margin:EdgeInsets.all(20.0),
       child: Column(
         children: [
-          // email(),
-          // password(), 
-          // submitButton()
+           email(),
+           password(), 
+           submitButton()
         ],),
     );
   }
   Widget email(){
-    return TextField(
+    return StreamBuilder(stream: bloc.email,
+     builder:(context, snapshot) {
+   return TextField(
+      onChanged: bloc.changeEmail,
       keyboardType:TextInputType.emailAddress,
       decoration: InputDecoration(
+        errorText: snapshot.error?.toString(),
         labelText: 'Email',
         hintText: "yourEmail@gmail.com",
       ),
 
     );
+     },)
+   ;
   }
   Widget password(){
-    return TextField(
+    return StreamBuilder(stream: bloc.password,
+    builder:(context, snapshot) {
+      return TextField(
+        onChanged: bloc.changePassword,
       obscureText: true,
       decoration:InputDecoration(
+        errorText: snapshot.error?.toString(),
         hintText: 'Password',
         labelText:'password'
       )
       
 
     );
+    },) ;
   }
   Widget submitButton(){
     return ElevatedButton(
