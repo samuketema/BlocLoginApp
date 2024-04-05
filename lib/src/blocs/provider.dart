c17fa4d5a624360db70451fb9ae74cc25d212bbd
Samuel <samuelketema01@gmail.com>
2024-04-05 23:09:55 +0300

    an error is solved by making the arguments nulluble and required

--- lib/src/blocs/provider.dart
+++ lib/src/blocs/provider.dart
@@ -1,14 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'bloc.dart';
 
 class Provider extends InheritedWidget{
   final bloc = Bloc();
 
-  Provider({key ,required Widget child})
+  Provider({Key? key , required Widget child})
    :super(key: key , child: child);
+
+     @override
   bool updateShouldNotify(_) => true;
 
   static Bloc of(context){
-    return (context.inheritFromWidgetOfExactType(Provider) as Provider).bloc;
+    return (context.dependOnInheritedWidgetOfExactType<Provider>()).bloc;
   }
 }
